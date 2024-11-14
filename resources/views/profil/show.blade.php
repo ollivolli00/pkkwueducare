c6808df6545048da7d5e9118006d12c89980cbbb
ollivolli00 <oliveworld00@gmail.com>
2024-11-14 08:04:40 +0700

    dropdown

--- resources/views/profil/show.blade.php
+++ resources/views/profil/show.blade.php
@@ -69,21 +69,21 @@
         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
             {{ Auth::user()->name }}
         </a>
-        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-        @if (Auth::user()->profile) <!-- Cek jika profil ada -->
-                <a href="{{ route('pengguna.show', Auth::user()->profile->id) }}">Profile</a>   
-            @else
-                <a href="{{ route('pengguna.create') }}">Profile</a>
-            @endif  
-        <a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </div>
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown" style="padding-left: 15px;">
+    @if (Auth::user()->profile) <!-- Cek jika profil ada -->
+        <a href="{{ route('pengguna.show', Auth::user()->profile->id) }}">Profile</a>   
+    @else
+        <a href="{{ route('pengguna.create') }}">Profile</a>
+    @endif  
+    <a class="dropdown-item" href="{{ route('logout') }}"
+       onclick="event.preventDefault();
+                 document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
+</div>
     @else
         <a href="{{ route('login') }}" style="color:white; padding: 10px 15px 12px; background: #7fad39;">Login</a>
     @endauth
